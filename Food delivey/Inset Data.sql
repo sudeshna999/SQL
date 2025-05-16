3e5ce0526870d001ecfae68129323b7ff51ea722
Sudeshna Dey <113450003+sudeshna999@users.noreply.github.com>
2025-05-16 11:05:30 +0530

    Update Inset Data.sql

--- Food delivey/Inset Data.sql
+++ Food delivey/Inset Data.sql
@@ -2,7 +2,7 @@
 -- dataset is very large so we used non GUI method to load the data
 -- load data using cmd
 
-LOAD DATA LOCAL INFILE "C:\\Users\\SUDESHNA\\OneDrive\\Sudeshna Dey\\IVY\\New folder\\Zomato Dataset Cleaned.csv" 
+LOAD DATA LOCAL INFILE "C:\\Users\\SUDESHNA\\OneDrive\\Sudeshna Dey\\IVY\\New folder\\Dataset Cleaned.csv" 
 INTO TABLE delivery										
 FIELDS TERMINATED BY ','											
 ENCLOSED BY '"'											
